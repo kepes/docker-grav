af7d1812f1a0354b12b325a6a4328879ee4dab76
kepes.peter <kepes.peter@codeplay.hu>
2018-08-31 16:45:02 +0200

    chown on every restart

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ if ! [ -e user/config/system.yaml ]; then
   echo >&2 "No userfiles found - copying now..."
   rsync -a /tmp/grav-$GRAV_VERSION/user/ /var/www/html/user
 fi
-chown -Rf ${APACHE_RUN_USER}:${APACHE_RUN_GROUP} user || true
+chown -Rf ${APACHE_RUN_USER}:${APACHE_RUN_GROUP} /var/www/html || true
 echo >&2 "Complete! Grav has been successfully installed"
 
 exec "$@"
